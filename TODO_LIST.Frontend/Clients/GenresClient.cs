80811ea1f82e4dc3253b84be103a60087c2a0c62
KacperJodjod <kacperosj2003@gmail.com>
2024-08-23 17:27:11 +0200

    All is Asynchronous now!
    not working yet

--- TODO_LIST.Frontend/Clients/GenresClient.cs
+++ TODO_LIST.Frontend/Clients/GenresClient.cs
@@ -4,32 +4,6 @@ namespace TODO_LIST.Frontend.Clients;
 
 public class GenresClient(HttpClient httpClient)
 {
-    private readonly List<Genre> genres = [
-        new() {
-            Id = 1,
-            GenreName = "Codzienne"
-        },
-        new() {
-            Id = 2,
-            GenreName = "Praca w domu"
-        },
-        new() {
-            Id = 3,
-            GenreName = "Nauka"
-        },
-        new() {
-            Id = 4,
-            GenreName = "Żywienie"
-        },
-        new() {
-            Id = 5,
-            GenreName = "Hobby"
-        },
-        new() {
-            Id = 6,
-            GenreName = "Znajomości"
-        }
-    ];
-
-    public Genre[] GetGenres() => genres.ToArray();
+    public async Task<Genre[]> GetGenresAsync() => 
+        await httpClient.GetFromJsonAsync<Genre[]>($"genres") ?? [];
 }
